8d3af3f95df95d3c08bcb7935ad31997109499b3
mcfilu <filip.1olszewski@gmail.com>
2023-08-21 17:56:18 +0100

    adding the chatbot to the pages and autoplaysinline to the oconceptinfo and oshotdetails pages

--- app/components/OconceptInfo.jsx
+++ app/components/OconceptInfo.jsx
@@ -22,7 +22,7 @@ const OconceptInfo = () => {
       <div className={`md:hidden flex flex-col items-center`}>
       <h1 className={`text-gold1 text-[5vh] text-center font-header`}>O-Concept for Her</h1>
       {/* <video src={boobs10} autoPlay loop muted className={` object-cover h-[100%] w-[100%]`}></video> */}
-      <video src={"/o-concept-animation.mp4"} autoPlay loop muted className={` object-cover h-[100%] w-[100%]`}></video>
+      <video src={"/o-concept-animation.mp4"} autoPlay loop muted playsInline className={` object-cover h-[100%] w-[100%]`}></video>
       <p className={`text-white font-poppins text-[2.1vh] text-left pl-[1vh] mt-[3vh] mb-[2vh] font-main`}>The O Concept™ for Her provides a personalized treatment plan tailored to address specific concerns related to women's sexual health. With a customized approach, it ensures optimal results for conditions such as low libido, orgasmic difficulties, or vaginal health issues. What sets the O Concept™ for Her apart is its unique combination of treatments, carefully designed to meet individual needs and provide comprehensive care.</p>
       <p className={`text-white font-poppins text-[2.1vh] text-left pl-[1vh] mt-[3vh] mb-[2vh] font-main`}>Dr. Sherif Wakil, renowned as a leading expert in Sexual Aesthetics, has been honored with numerous national and global awards for his groundbreaking work in sexual and reproductive health. His expertise and dedication have earned him the distinguished title of "God Father of Sexual Aesthetics." Patients can rely on his exceptional knowledge and experience, knowing that they are in the hands of a respected professional who is committed to providing the highest quality care for women's sexual well-being.</p>
             
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head>
+      {/* <!-- Google tag (gtag.js) --> */}
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NVHFPSMTMM"></script>
+    
+
+    
+      </Head>
+      <body className={inter.className}>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KFZWGS2"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
+
+        {children}
+        <script type="text/javascript" src="https://launcher.enquirybot.com/index.js" data-bot-id="a243d8ad-44a1-4441-aecd-b9b2a08ff886"></script>
+        </body>
     </html>
   )
 }
