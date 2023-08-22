6d2330268f20d9375e4926c67b51ed81833291b4
mcfilu <filip.1olszewski@gmail.com>
2023-08-22 14:08:18 +0100

    updated code with google tag manager aswell, and enquriy bot in <Script> tag " git push ;

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import GoogleAnalytics from './GoogleAnalytics'
+import GoogleTagManager from '@magicul/next-google-tag-manager';
 import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,12 +20,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <GoogleAnalytics/>
+      
+      
       <body className={inter.className}>
-        
-
+        <GoogleAnalytics/>
+        <GoogleTagManager id="GTM-KFZWGS2" />
         {children}
-        <script type="text/javascript" src="https://launcher.enquirybot.com/index.js" data-bot-id="a243d8ad-44a1-4441-aecd-b9b2a08ff886"></script>
+        <Script strategy="afterInteractive" data-bot-id="a243d8ad-44a1-4441-aecd-b9b2a08ff886" src="https://launcher.enquirybot.com/index.js"></Script>
+        {/* <script type="text/javascript" src="" data-bot-id="a243d8ad-44a1-4441-aecd-b9b2a08ff886"></script> */}
         </body>
     </html>
   )
