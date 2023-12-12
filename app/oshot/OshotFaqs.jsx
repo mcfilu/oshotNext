9a1895d1d6c70889658d5b535a4f74ff39cbe944
mcfilu <filip.1olszewski@gmail.com>
2023-12-12 09:59:44 +0100

    changing the oshot faqs video and o concept info components according to the pdf from seo people

--- app/oshot/OshotFaqs.jsx
+++ app/oshot/OshotFaqs.jsx
@@ -26,11 +26,11 @@ const OshotFaqs = () => {
                 <div className={`flex flex-col md:ml-[5%]`} onClick={() => {setSecond(!openSecond)}}>
                     <div className={`flex flex-row justify-left`}>
                         <i className={`fa-regular ${openSecond ? 'fa-square-minus' : 'fa-square-plus'} mr-4 text-gold1 text-[3vh] flex items-center`}></i>
-                        <h2 className={`${openSecond ? 'text-white' : 'text-white'} font-main text-[2.8vh]`}>Am i suitable for this treatment?</h2>
+                        <h2 className={`${openSecond ? 'text-white' : 'text-white'} font-main text-[2.8vh]`}>Am I suitable for the O-Shot® injection treatment?</h2>
                     </div>
                     <div className={`flex flex-col ${openSecond ? '' : 'hidden'} md:w-[40vw] mt-[2vh] mb-[1vh] ml-[5%]`}>
                         <p className={`text-white text-[2.1vh] text-justify font-main`}>
-                        There are a number of sexual arousal disorders that the O-Shot® can be used to address. It is estimated that as many as 1 in 20 women suffers from Female Sexual Arousal Disorder, which means that, while they may want to have sex, they find it difficult to get aroused, experience pleasure or achieve orgasm. 
+                        There are a number of sexual arousal disorders that the O-Shot® can be used to address. It is estimated that as many as 1 in 20 women suffer from Female Sexual Arousal Disorder, which means that, while they may want to have sex, they find it difficult to get aroused, experience pleasure or achieve orgasm.
                         </p>
 
                         <p className={`text-white text-[2.1vh] text-justify font-main mt-[2vh]`}>Around 5% of women are also thought to suffer from Female Orgasmic Disorder, where they can become aroused but have difficulty achieving orgasm. Some women also suffer from pain during and after sex, which is not from decreased lubrication or vaginal spasm, some suffer from stress urinary incontinence especially after delivery, If you have any of these problems then the O-Shot® could be a solution for you. As with any treatment, you will need to have a consultation with Dr Wakil first in order to assess your suitability and rule out any contraindications.</p>
@@ -95,7 +95,7 @@ const OshotFaqs = () => {
                             <li className={`flex items-center`}><i  className={`mr-4 fa-solid fa-circle text-[12px] text-gold1`}></i>Great improvement for Lichen Sclerosus conditions</li>
                             <li className={`flex items-center`}><i  className={`mr-4 fa-solid fa-circle text-[12px] text-gold1`}></i>Stops the tears, lacerations that accompanies Lichen Sclerosus condition</li>
                         </ul>
-                        <p className={`text-white text-[2.1vh] font-main`}>Please be advised that these results vary between patients according to age, medical condition, hormonal levels, social habits and the results are not guaranteed.</p>
+                        <p className={`text-white text-[2.1vh] font-main`}>Please be advised that these results vary between patients according to age, medical condition, hormonal levels, social habits and the results are not guaranteed. Check out our O-Shot® reviews below to learn more about our results.</p>
                     </div>
                 </div>
                 <hr className={`border-gold1 mt-[1.4vh] mb-[1.4vh] w-[90%] mx-auto`}></hr>
